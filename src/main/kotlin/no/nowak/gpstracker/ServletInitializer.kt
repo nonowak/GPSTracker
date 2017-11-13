5ce6ad185be4aa0f7c5e80a6ec2f4c80100efed4
Norbert Nowak <no.nowak@outlook.com>
2017-11-13 08:16:21 +0100

    init

---
+++ src/main/kotlin/no/nowak/gpstracker/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package no.nowak.gpstracker
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder) : SpringApplicationBuilder {
+		return application.sources(GpstrackerApplication::class.java)
+	}
+
+}
