6d28c32fcffc3f83dc9f2d45f0bcacb6250a53e4
Norbert Nowak <no.nowak@outlook.com>
2017-11-19 21:25:02 +0100

    GPST-4 working

--- src/main/kotlin/no/nowak/gpstracker/core/user/UserApi.kt
+++ src/main/kotlin/no/nowak/gpstracker/core/user/UserApi.kt
@@ -1,9 +1,11 @@
 package no.nowak.gpstracker.core.user
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/user")
 interface UserApi {
 
+    @GetMapping
     fun login()
 }
