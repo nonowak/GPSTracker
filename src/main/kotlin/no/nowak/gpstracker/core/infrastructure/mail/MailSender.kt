84322ff715ef6401cc37438950074eb401c3a470
nonowak@outlook.com <nonowak@outlook.com>
2017-12-10 00:54:54 +0100

    GPST-35 mail sender

--- src/main/kotlin/no/nowak/gpstracker/core/infrastructure/mail/MailSender.kt
+++ src/main/kotlin/no/nowak/gpstracker/core/infrastructure/mail/MailSender.kt
@@ -1,2 +1,24 @@
 package no.nowak.gpstracker.core.infrastructure.mail
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMailMessage
+import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.stereotype.Service
+
+@Service
+class MailSender(val javaMailSender: JavaMailSender){
+
+    @Value("\${spring.mail.username}")
+    val sender: String = ""
+
+    fun sendMail(subject: String, content: String, recipient: String){
+        val mail = javaMailSender.createMimeMessage()
+        var helper = MimeMessageHelper(mail)
+        helper.setFrom(sender)
+        helper.setSubject(subject)
+        helper.setText(content)
+        helper.setTo(recipient)
+        javaMailSender.send(mail)
+    }
+}
