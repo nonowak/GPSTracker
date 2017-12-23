771835c2a714ffb79183e7fcdea0df72ec9efd2a
Norbert Nowak <no.nowak@outlook.com>
2017-12-23 11:51:06 +0100

    GPST-43 parse incoming time

--- Service/src/main/kotlin/no/nowak/gpstracker/measurement/MeasurementGPSService.kt
+++ Service/src/main/kotlin/no/nowak/gpstracker/measurement/MeasurementGPSService.kt
@@ -8,6 +8,9 @@ import com.google.maps.model.LocationType
 import no.nowak.gpstracker.measurement.dto.MeasurementGPSDTO
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 class MeasurementGPSService(private val measurementGPSRepository: MeasurementGPSRepository) {
@@ -15,17 +18,21 @@ class MeasurementGPSService(private val measurementGPSRepository: MeasurementGPS
     @Value("\${google.key}")
     private val key: String = ""
 
+    private var DATE_TIME_FORMATER = DateTimeFormatter.ofPattern("uuuuMMddHHmmss")
+
     fun addMeasurement(measurementGPSDTO: MeasurementGPSDTO) {
         getAddressFromLatLng(LatLng(measurementGPSDTO.latitude, measurementGPSDTO.longitude))
+        val localDateTime = LocalDateTime.parse(measurementGPSDTO.measurementTime, DATE_TIME_FORMATER)
+        print(localDateTime)
     }
 
     private fun getAddressFromLatLng(latLng: LatLng) {
-        val context = GeoApiContext.Builder()
-                .apiKey(key)
-                .build()
-        val geocodingResult: List<GeocodingResult> = GeocodingApi.reverseGeocode(context, latLng)
-                .locationType(LocationType.ROOFTOP)
-                .await().toList()
-        print(geocodingResult)
+//        val context = GeoApiContext.Builder()
+//                .apiKey(key)
+//                .build()
+//        val geocodingResult: List<GeocodingResult> = GeocodingApi.reverseGeocode(context, latLng)
+//                .locationType(LocationType.ROOFTOP)
+//                .await().toList()
+//        print(geocodingResult)
     }
 }
