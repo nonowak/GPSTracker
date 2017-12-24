51c5e0966b4a2822fa03b4077a8b8890b260625f
Norbert Nowak <no.nowak@outlook.com>
2017-12-24 16:41:46 +0100

    GPST-63 time mapper probably works

---
+++ Service/src/main/kotlin/no/nowak/core/infrastructure/converters/JacksonConfig.kt
@@ -0,0 +1,20 @@
+package no.nowak.core.infrastructure.converters
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+class JacksonConfig : WebMvcConfigurerAdapter() {
+    override fun extendMessageConverters(converters: List<HttpMessageConverter<*>>?) {
+        for (converter in converters!!) {
+            if (converter is MappingJackson2HttpMessageConverter) {
+                val objectMapper = converter.objectMapper
+                objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                break
+            }
+        }
+    }
+}
--- Service/src/main/kotlin/no/nowak/gpstracker/measurement/dto/MeasurementResponseDTO.kt
+++ Service/src/main/kotlin/no/nowak/gpstracker/measurement/dto/MeasurementResponseDTO.kt
@@ -3,17 +3,19 @@ package no.nowak.gpstracker.measurement.dto
 import com.google.maps.model.LatLng
 import no.nowak.gpstracker.google.GoogleAddress
 import no.nowak.gpstracker.measurement.MeasurementGPS
+import java.time.LocalDate
+import java.time.LocalTime
 
 data class MeasurementResponseDTO(
         val address: GoogleAddress,
         val latLng: LatLng,
-        val measurementTime: String,
-        val measurementDate: String
+        val measurementTime: LocalTime,
+        val measurementDate: LocalDate
 ) {
     constructor(measurementGPS: MeasurementGPS) : this(
             latLng = measurementGPS.latLng,
             address = measurementGPS.address,
-            measurementTime = measurementGPS.time.toString(),
-            measurementDate = measurementGPS.measurementDate.date.toString()
+            measurementTime = measurementGPS.time,
+            measurementDate = measurementGPS.measurementDate.date
     )
 }
